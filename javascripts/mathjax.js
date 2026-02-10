b006f58f6ef280d93d5038440c652763061b85b8
Emil Frisk <emilwfrisk@gmail.com>
2026-02-10 17:04:59 +0100

    Deployed bdcb9aa with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,23 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  chtml: {
+    displayAlign: "left",
+    displayIndent: "0em"
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
